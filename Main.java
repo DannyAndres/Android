a6ba5d55ccd6e5f1d262b5cd415d8b399307fde2
Danny Andres <danny.aguilar@usach.cl>
2016-11-23 02:14:20 -0300

    tutorial

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args){
 
-		System.out.println("hola mundo");
+		String mensaje, nombre;
+		mensaje = "probando java";
+		nombre = "danny ";
+		System.out.println(nombre+mensaje);
 	
 	}
 
