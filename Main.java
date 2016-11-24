751901ddafdd0a8c81f1f3cd8be305e80b96b618
Danny Andres <danny.aguilar@usach.cl>
2016-11-24 02:39:36 -0300

    24 nov

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args){
 
-		String mensaje, nombre;
-		mensaje = "probando java";
-		nombre = "danny ";
-		System.out.println(nombre+mensaje);
-	
+		int edad;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("cual es tu edad?");
+		edad.nextInt();
+		System.out.println("tu nombre es "+ ());
+
 	}
 
 }
