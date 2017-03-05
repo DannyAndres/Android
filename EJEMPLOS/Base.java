6ce7421670c99431ab32c091f467621a3085b161
dannyandres <danny.aguilar@usach.cl>
2017-03-05 04:06:27 -0300

    ejemplo

---
+++ EJEMPLOS/Base.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;import java.text.DecimalFormat;
+
+public class Base{
+	public static void main(String[] args){
+		System.out.println("hola a todos");
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.0");
+		int[] m = {1,2,3};
+		int[] n = new int[3];
+		n = [1,2,3];
+		//n es igual a m
+	}
+}
